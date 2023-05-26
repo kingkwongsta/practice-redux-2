57f046713dee4ae7fe7b74c786ec06ae5185a8a7
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-25 23:16:26 -0700

    enabled test counter to show same data

--- src/app/components/TestCounter.jsx
+++ src/app/components/TestCounter.jsx
@@ -1,7 +1,10 @@
+import { useSelector, useDispatch } from "react-redux";
+
 export default function TestCounter() {
+  const count = useSelector((state) => state.counter.value);
   return (
     <div>
-      <h2>This is the count</h2>
+      <h2>This is the count {count}</h2>
     </div>
   );
 }
