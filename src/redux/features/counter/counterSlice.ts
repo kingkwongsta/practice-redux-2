d6306e1eed96b47c5ce993cec5e3bb150a353478
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-25 23:05:18 -0700

    setup slice of store for counter

---
+++ src/redux/features/counter/counterSlice.ts
@@ -0,0 +1,27 @@
+"use client";
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+export const CounterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
